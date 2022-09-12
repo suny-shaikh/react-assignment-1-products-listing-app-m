b094f89e7ce9ddbfc722f4d109b0eec3f7cb4788
suny-shaikh <mdsunyshaikh@gmail.com>
2022-09-12 15:07:14 +0600

    update style and product style

--- src/components/Product.js
+++ src/components/Product.js
@@ -8,8 +8,8 @@ const Product = ({ product }) => {
       <img src={image} alt="" />
       <div className="product__details">
         <h4 className="product__title">{title}</h4>
-        <p>Price: $ {price}</p>
-        <p>Rating: {rating.rate}/5</p>
+        <p className="product__price">Price: $ {price}</p>
+        <p className="product__rating">Rating: {rating.rate}/5</p>
         <p className="product__desc">Description: {description}</p>
         <button className="product__btn btn">Add to cart</button>
       </div>
